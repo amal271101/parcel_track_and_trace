c839c5c4a675e58f49239977053a204fc465cae1
Amal <if20b223@technikum-wien.at>
2022-10-24 18:35:51 +0200

    fix

--- target/generated-sources/annotations/at/fhtw/swen3/services/mapper/TruckMapperImpl.java
+++ target/generated-sources/annotations/at/fhtw/swen3/services/mapper/TruckMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-10-23T22:52:20+0200",
+    date = "2022-10-24T18:35:22+0200",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 18.0.2.1 (Oracle Corporation)"
 )
 public class TruckMapperImpl implements TruckMapper {
