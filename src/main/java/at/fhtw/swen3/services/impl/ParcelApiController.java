edebff781e8c9343e259e3c65f610f856c8ed0df
Amal <if20b223@technikum-wien.at>
2022-10-24 18:45:49 +0200

    fix

--- src/main/java/at/fhtw/swen3/services/impl/ParcelApiController.java
+++ src/main/java/at/fhtw/swen3/services/impl/ParcelApiController.java
@@ -15,6 +15,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.context.request.NativeWebRequest;
 
 import java.time.OffsetDateTime;
@@ -24,7 +25,7 @@ import java.util.Optional;
 import java.util.Set;
 import javax.annotation.Generated;
 import javax.validation.*;
-
+@RequestMapping("${openapi.parcelLogisticsService.base-path:}")
 @Generated(value = "org.openapitools.codegen.languages.SpringCodegen", date = "2022-09-24T11:01:08.846404Z[Etc/UTC]")
 @Controller
 public class ParcelApiController implements ParcelApi {
