8f84bd2a3130c09b534dbf5b204334b21eff5fbd
Amal <if20b223@technikum-wien.at>
2022-11-25 08:55:20 +0100

    added aws environmet variables

--- target/generated-sources/annotations/at/fhtw/swen3/services/mapper/TruckMapperImpl.java
+++ target/generated-sources/annotations/at/fhtw/swen3/services/mapper/TruckMapperImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-11-19T22:34:51+0100",
+    date = "2022-11-21T15:57:40+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 18.0.2.1 (Oracle Corporation)"
 )
 public class TruckMapperImpl implements TruckMapper {
