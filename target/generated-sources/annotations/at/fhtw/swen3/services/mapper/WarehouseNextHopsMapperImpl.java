46fc83f149b386e50b7ce6247388e5ca56822c22
amalmostafa <if20b223@technikum-wien.at>
2022-12-29 14:59:08 +0100

    commit

--- target/generated-sources/annotations/at/fhtw/swen3/services/mapper/WarehouseNextHopsMapperImpl.java
+++ target/generated-sources/annotations/at/fhtw/swen3/services/mapper/WarehouseNextHopsMapperImpl.java
@@ -10,7 +10,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2022-12-29T14:39:46+0100",
+    date = "2022-12-29T14:56:36+0100",
     comments = "version: 1.4.2.Final, compiler: javac, environment: Java 17.0.5 (Amazon.com Inc.)"
 )
 public class WarehouseNextHopsMapperImpl implements WarehouseNextHopsMapper {
